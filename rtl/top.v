7c481a3e64890f90f6f0be5494595fd4b37d04b1
Boyd Thomas <boydbt48@gmail.com>
2024-02-06 17:18:05 -0500

    Added blinky test

--- rtl/top.v
+++ rtl/top.v
@@ -1,11 +1,28 @@
 module top (
     input clk30,
     input [1:0] user_buttons,
-    output [6:0] user_leds_en,
-    output [2:0] user_leds_color
+    output reg [2:0] user_leds_color,
+    output reg [6:0] user_leds_en
 );
+ 
+    reg en;
+    reg [2:0] color;
+    reg [32:0] counter;
 
-    assign user_leds_color = 3'b110;
-    assign user_leds_en = {user_buttons[1], {3{~user_buttons[1], user_buttons[1]}}};
+    always @(*) begin
+        user_leds_en = {en, {3{~en, en}}};
+        user_leds_color = color;
+    end
 
+    always @(posedge clk30) begin
+        counter <= counter + 1;
+
+        if (counter >= 15000000) begin
+            counter <= 0;
+            en <= ~en;
+            color <= color + 1;
+        end
+    end
+    
 endmodule
+
